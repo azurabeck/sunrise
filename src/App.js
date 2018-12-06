afd15e80c519a79f99ed0d879722522bf7ac61f6
Rebecca <rebecca@psafe.com>
2018-12-06 18:13:20 -0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// IMAGE IMPORT 
+import logo from './image/logo_color.png';
+import map from './image/map.png';
+import fav from './image/favIcon.svg';
+
+// EXTERNAL LINK
+import { Row, Col } from 'react-bootstrap'
+import MaterialIcon from 'material-icons-react'
+
 class App extends Component {
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+            <div className='brand'>                  
+                <img src={logo} alt=''/>                  
+            </div>
+
+            <Row className='buildingRow'>
+                  <Col md={4} className='buildingBlock'>
+                  
+                        <label>Estamos em contrução</label>
+                  
+                  </Col>
+                  <Col md={8} className='contactInformation'>
+
+                        <MaterialIcon id='arrow' icon='arrow_right' color='#ea9220' />                  
+                        <label>Mas você pode entrar em contato:</label>
+
+                        <ul>
+                              <li>
+                                  <span><MaterialIcon icon="call" color='#a9a6a6' /></span>
+                                  <span>(21) 9 8868 - 1862</span>
+                              </li>
+                              <li>
+                                  <span><MaterialIcon icon="business" color='#a9a6a6' /></span>
+                                  <span> (21) 3796-8428</span>
+                              </li>
+                              <li className='mb-30'>
+                                  <span><MaterialIcon icon="email" color='#a9a6a6'/></span>
+                                  <span> marcellostasi@sunrise.com.br</span>
+                              </li>
+                              <li>
+                                  <span><MaterialIcon icon="location_on" color='#a9a6a6'/></span>
+                                  <span> Rua Odilon Duarte Braga - n192 - sala 305. Recreio dos Bandeirantes</span>
+                              </li>
+                        </ul>
+                  
+                  </Col>
+
+            </Row>
+
+            <footer>       
+                      <img src={fav} className='fav' alt=''/> 
+            </footer>
+
       </div>
     );
   }
 }
 
-export default App;
+export default (App)
